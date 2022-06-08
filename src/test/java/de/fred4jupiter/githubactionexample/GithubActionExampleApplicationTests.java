ba879c87f9b26b094ea597a7b5f268a5155fe927
Staehler, Michael <michael.staehler@opitz-consulting.com>
2022-06-08 11:13:32 +0200

    initial commit

---
+++ src/test/java/de/fred4jupiter/githubactionexample/GithubActionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fred4jupiter.githubactionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
